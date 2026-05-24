323bd9de8ed5deb1f8515eea5cb2924be02bb3a5
Adrian - HomePC <adrianelowerycoding@gmail.com>
2026-05-24 06:33:37 -0500

    ~ I've recently converted the file to OOP and added saving selections as a file. Currenlty I'm having problems understanding the whole 'self' stuff and how it affects the current layout and logic. The big issue now is that the 'c' key won't clear the drawing and I'm not sure why. Chat is trying to get me to refactor a lot of the code and I just don't know about that.
    Try to take time to really figure out what's going on and the logic
    before you make any big changes. I think the problem can be solved
    simpler than what Chat is suggesting.

--- cv2Test.py
+++ cv2Test.py
@@ -17,7 +17,6 @@ class ScreenshotDraw:
     def __init__(self, image_path):
         self.top_left_corner=[]
         self.bottom_right_corner=[]
-        # r"C:\Users\adria\Documents\Coding\Python\Big Projects\SOP Automater\SOP-Automater\Screenshots\test.png"
         self.image = cv2.imread(image_path)
         self.tempImage = self.image.copy()
         
@@ -31,7 +30,7 @@ class ScreenshotDraw:
             self.bottom_right_corner = [(x,y)]   
             # Draw the rectangle
             cv2.rectangle(self.image, self.top_left_corner[0], self.bottom_right_corner[0], (0,255,0),2, 8)
-
+            # This updates the screen immediately after drawing
             cv2.imshow("Window", self.image)
 
 
@@ -47,14 +46,25 @@ class ScreenshotDraw:
 
         k=0
 
-        while k!=113: # q key ends the loop
-            # Actually running/showing/opening the Window we created
+        while k!=113 : # q and s keys end the loop
+            self.increase = 0
+            # Initially running/showing/opening the Window we created
             cv2.imshow("Window", self.image)
             k = cv2.waitKey(0)
 
-            if (k == 99): # c key clears the current selection
-                image = self.tempImage.copy()
-                cv2.imshow("Window", image)
+            if (k == 99): # c key clears the current selection and replaces it with copy image
+                tempImage = self.tempImage.copy()
+                cv2.imshow("Window", tempImage)
+
+            if (k == 115): # s saves the selection
+                self.increment = self.increase + 1
+                x1, y1 = self.top_left_corner[0]
+                x2, y2 = self.bottom_right_corner[0]
+
+                cv2.imwrite(rf"C:\Users\adria\Documents\Coding\Python\Big Projects\SOP Automater\SOP-Automater\Screenshots\screenshot{self.increment}.png",
+                             self.tempImage[min(y1,y2):max(y1,y2), min(x1,x2):max(x1,x2)]
+                             )
+                print("Saved screenshot selection")
 
         cv2.destroyAllWindows()
 
@@ -62,4 +72,12 @@ class ScreenshotDraw:
 # while True:
 #     k = cv2.waitKey(0) & 0xFF
 #     if k == ord('q'):
+
+test = ScreenshotDraw(
+    r"C:\Users\adria\Documents\Coding\Python\Big Projects\SOP Automater\SOP-Automater\Screenshots\test.png"
+    )
+
+test.run()
+
+
 
