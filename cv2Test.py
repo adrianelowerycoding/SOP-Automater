77c7a64f5678b91a21ff949fd0490efc67e1a5d9
Adrian - HomePC <adrianelowerycoding@gmail.com>
2026-05-24 20:44:40 -0500

    REAL Initial Commit - Comments/Guides added

--- cv2Test.py
+++ cv2Test.py
@@ -16,7 +16,7 @@ tempImage = image.copy()
  
 # function which will be called on mouse input
 def drawRectangle(action, x, y, flags, *userdata):
-  # Referencing global variables
+  # Referencing global variables; making them accesible inside this function
   global top_left_corner, bottom_right_corner
   # Mark the top left corner when left mouse button is pressed
   if action == cv2.EVENT_LBUTTONDOWN:
@@ -24,8 +24,10 @@ def drawRectangle(action, x, y, flags, *userdata):
     # When left mouse button is released, mark bottom right corner
   elif action == cv2.EVENT_LBUTTONUP:
     bottom_right_corner = [(x,y)]   
-    # Draw the rectangle
+    # Draw the rectangle; it applies the drawing to the image IN MEMORY; changes not seen because changed image not yet uploaded
+    # to window.
     cv2.rectangle(image, top_left_corner[0], bottom_right_corner[0], (0,255,0),2, 8)
+    # Window is recreated and new changed image is uploaded into it and therefore displayed on screen.
     cv2.imshow("Window",image)
 
 # Creating the Window the screenshot sits in. 
@@ -33,21 +35,26 @@ def drawRectangle(action, x, y, flags, *userdata):
 cv2.namedWindow("Window", cv2.WINDOW_NORMAL)
 # Setting the window's size to full screen. 
 cv2.setWindowProperty("Window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-# Listens for left button mouse press and release
+# Starts listener for left button mouse press and release
 cv2.setMouseCallback("Window", drawRectangle)
 
 k=0
-
-while k!=113: 
+# After all above stuff has been loaded into memory, we jump into while loop to get program running.
+while k!=113: #q quits the loop and the program
     # Actually running/showing/opening the Window we created
     cv2.imshow("Window", image)
-    k = cv2.waitKey(0)
+    k = cv2.waitKey(0) # Stops loop and waits for a key to be pressed. Mouse listener is not stopped and is still listening.
 
-    if (k == 99):
-        image = tempImage.copy()
+    if (k == 99): #c replaces original image w/ copy, "wiping" the board clean
+        image = tempImage.copy() # Must be COPY because it willl only clear one time if not 
         cv2.imshow("Window", image)
 
 cv2.destroyAllWindows()
 
+
+# Make user choose screenshot naming convention. 
+# Make screenshot names increment. 
+# Choose selection created by rectangle. 
+# Create 's' command to save screenshot. 
 
 
